61299bc2f0849410073e7a78bb57a1b13d8b0ff1
Assan Sanogo <assansanogo@gmail.com>
2022-03-25 21:49:25 -0700

    Update streamliner.py

--- streamliner.py
+++ streamliner.py
@@ -15,7 +15,7 @@ if __name__=='__main__':
         subject='Sending clean data from Streamline datascience',
         html_content='<strong>But it was sent from Python</strong>'
     )
-
+    
     config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
     report_csv = glob2.glob(os.path.join(os.getcwd(), "*.csv"))
     pdfkit.from_file(report_csv, './report.pdf', configuration=config)
@@ -24,15 +24,41 @@ if __name__=='__main__':
         data = f.read()
         f.close()
     encoded_file = base64.b64encode(data).decode()
+ 
+    
+    data = {
+    "personalizations": [
+        {
+        "to": [
+            {
+            "email": "test@example.com"
+            }
+        ],
+        "subject": "Sending with SendGrid is Fun"
+        }
+    ],
+    "from": {
+        "email": "test@example.com"
+    },
+    "content": [
+        {
+            "type": "text/plain",
+            "value": "and easy to do anywhere, even with Python"
+        }
+    ],
+    "attachments": [
+    {
+        "content": f"{encoded_file}",
+        "content_id": "ii_139db99fdb5c3704",
+        "disposition": "inline",
+        "filename": "file1.jpg",
+        "name": "report",
+        "type": "pdf"
+        }
+        ]
+    }
+    response = sg.client.mail.send.post(request_body=data)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
-    attachedFile = Attachment(
-        FileContent(encoded_file),
-        FileName('./report.pdf'),
-        FileType('application/pdf'),
-        Disposition('attachment')
-    )
-    message.attachment = attachedFile
-
-    sg = SendGridAPIClient(sys.argv[1])
-    response = sg.send(message)
-    print(response.status_code, response.body, response.headers)
