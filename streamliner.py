97321c16a4940151caacf3142a5b2bfa51a74dd7
Assan Sanogo <assansanogo@gmail.com>
2022-03-25 21:28:41 -0700

    arguments passed to streamliner

--- streamliner.py
+++ streamliner.py
@@ -2,35 +2,37 @@ import os
 import glob2
 import base64
 import pdfkit
+import
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
 
+if __name__=='main':
+    
+    message = Mail(
+        from_email=sys.argv[2],
+        to_emails=sys.argv[3],
+        subject='Sending clean data from Streamline datascience',
+        html_content='<strong>But it was sent from Python</strong>'
+    )
 
-message = Mail(
-    from_email=os.environ.get('SENDER_EMAIL'),
-    to_emails=os.environ.get('DEST_EMAIL'),
-    subject='Sending clean data from Streamline datascience',
-    html_content='<strong>But it was sent from Python</strong>'
-)
+    config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+    report_csv = glob2.glob(os.path.join(os.getcwd(), "*.csv"))
+    pdfkit.from_file(report_csv, './report.pdf', configuration=config)
 
-config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-report_csv = glob2.glob(os.path.join(os.getcwd(), "*.csv"))
-pdfkit.from_file(report_csv, './report.pdf', configuration=config)
+    with open('./report.pdf', 'rb') as f:
+        data = f.read()
+        f.close()
+    encoded_file = base64.b64encode(data).decode()
 
-with open('./report.pdf', 'rb') as f:
-    data = f.read()
-    f.close()
-encoded_file = base64.b64encode(data).decode()
+    attachedFile = Attachment(
+        FileContent(encoded_file),
+        FileName('report.pdf'),
+        FileType('application/pdf'),
+        Disposition('attachment')
+    )
+    message.attachment = attachedFile
 
-attachedFile = Attachment(
-    FileContent(encoded_file),
-    FileName('report.pdf'),
-    FileType('application/pdf'),
-    Disposition('attachment')
-)
-message.attachment = attachedFile
-print(os.environ.get('SENDGRID_API_KEY'))
-sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-response = sg.send(message)
-print(response.status_code, response.body, response.headers)
+    sg = SendGridAPIClient(sys.argv[1])
+    response = sg.send(message)
+    print(response.status_code, response.body, response.headers)
