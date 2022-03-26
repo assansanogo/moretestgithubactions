d5b603b6f9ae491fccb4c689959f084ad71bf472
Assan Sanogo <assansanogo@gmail.com>
2022-03-25 21:17:44 -0700

    Update streamliner.py

--- streamliner.py
+++ streamliner.py
@@ -30,7 +30,7 @@ attachedFile = Attachment(
     Disposition('attachment')
 )
 message.attachment = attachedFile
-
+print(os.environ.get('SENDGRID_API_KEY'))
 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
 response = sg.send(message)
 print(response.status_code, response.body, response.headers)
