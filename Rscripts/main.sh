070c353922c4409bdfcb6cab60b9f8961ca7a4ba
Assan Sanogo <assansanogo@gmail.com>
2022-03-29 23:59:27 -0700

    Update main.sh

--- Rscripts/main.sh
+++ Rscripts/main.sh
@@ -1,7 +1,9 @@
 !/bin/sh -l
 
-Rscript --vanilla reporter.r "$DATE" "$AGGREGATION" "$COUNTRY"
+Rscript --vanilla reporter.r "$DATE" "$AGGREGATION" "$COUNTRY
 
 output_text=cat $(ls *.csv)
+
+echo $output_text
 
 echo "::set-output name=summarized::$output_text"
