673fcf530bafae9f199409ae02c0fca4f183bd34
Assan Sanogo <assansanogo@gmail.com>
2022-04-01 13:41:55 -0700

    Update requirements.r

--- Rscripts/requirements.r
+++ Rscripts/requirements.r
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-# packages that are stored on github (not on the CRAN)
+# packages that are stored on the CRAN & then on github (not on the CRAN)
 if(!interactive()){
+install.packages('prob')
 devtools::install_github("assansanogo/moretestgithubactions") #streamline repository(JH analysis)
 devtools::install_github("hadley/emo") #emoji repository
 }
