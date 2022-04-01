e9e9c6ff77e5ca8d1776acf4b9517733bd258498
Assan Sanogo <assansanogo@gmail.com>
2022-03-31 17:31:01 -0700

    Update main.sh

--- Rscripts/main.sh
+++ Rscripts/main.sh
@@ -2,8 +2,5 @@
 
 Rscript --vanilla reporter.r "$DATE" "$AGGREGATION" "$COUNTRY"
 
-echo "$PROCESSED_REPORT_PATH"
-echo "$TOTAL_REPORT_PATH"
-echo "$OUTPUT_REPORT_PATH"
 
 Rscript --vanilla saver.r "$PROCESSED_REPORT_PATH" "$TOTAL_REPORT_PATH" "$OUTPUT_REPORT_PATH"
