c44c2f374539f381ffda9d5da34a7121b571b6af
Assan Sanogo <assansanogo@gmail.com>
2022-03-31 21:00:13 -0700

    Update saver.r

--- Rscripts/saver.r
+++ Rscripts/saver.r
@@ -22,16 +22,32 @@ if(!interactive()){
     #left join
     total_deaths_confirmed <- merge(total_deaths_confirmed, 
                                     deaths_confirmed, 
-                                    by.x = "State", 
-                                    by.y = "Sate", 
+                                    by.x = "Province_State", 
+                                    by.y = "Province_State", 
                                     all.x = TRUE, 
                                     all.y = FALSE)
     return(total_deaths_confirmed)
     },
     error=function(cond) {
+        
+        
+        
+                csv_countries_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
+                dataframe_countries<-read.delim(csv_countries_filename, sep=',')
+        
                 message(paste("The file (consolidated) you are trying to merge with, may not exist"))
                 message("Original error message:",cond)
                 # Choose a return value in case of error
+        
+        
+               total_deaths_confirmed <- merge(dataframe_countries, 
+                            deaths_confirmed, 
+                            by.x = "Province_State", 
+                            by.y = "Province_State", 
+                            all.x = TRUE, 
+                            all.y = FALSE)
+        
+        
                 return(deaths_confirmed)
             })
     return(out)
