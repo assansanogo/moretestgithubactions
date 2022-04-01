5e16c92bf00871b43f071d686c7254e29f50eea1
Assan Sanogo <assansanogo@gmail.com>
2022-04-01 11:48:53 -0700

    Update saver.r

--- Rscripts/saver.r
+++ Rscripts/saver.r
@@ -4,58 +4,84 @@
 library(StreamlinR)  
 library(emo)
 
-if(!interactive()){
-    args = commandArgs(trailingOnly=TRUE)
-    
-    concatener <- function(){
-        
-    deaths_confirmed <- read.delim(args[1], sep=',')
 
+
+concatener <- function(args){
+    # current report    
+    deaths_confirmed <- read.delim(args[1], sep=',')
+    
+    # external dependency (list of countries)
     csv_countries_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
     dataframe_countries<-read.delim(csv_countries_filename, sep=',')
 
-
-    out <- tryCatch({ 
+    # running report
     total_deaths_confirmed <- read.delim(args[2], sep=',')
+    
+    out <- tryCatch({ 
+        
 
-    print(colnames(total_deaths_confirmed ))
-    print(colnames(deaths_confirmed))
-        
-        
-        
-    #left join
-    total_deaths_confirmed <- merge(total_deaths_confirmed, 
-                                    deaths_confirmed, 
-                                    by = "Province_State", 
-                                    all.x = TRUE, 
-                                    all.y = FALSE)
-    return(total_deaths_confirmed)
-    },
-    error=function(cond) {
-        
-        
-        
-                csv_countries_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
-                dataframe_countries<-read.delim(csv_countries_filename, sep=',')
-        
-                message(paste("The file (consolidated) you are trying to merge with, may not exist"))
-                message("Original error message:",cond)
-                # Choose a return value in case of error
-        
-        
-               total_deaths_confirmed <- merge(dataframe_countries, 
-                            deaths_confirmed, 
-                            by = "Province_State", 
-                            all.x = TRUE, 
-                            all.y = FALSE)
-        
-        
-                return(deaths_confirmed)
-            })
-    return(out)
+        print(colnames(total_deaths_confirmed ))
+        print(colnames(deaths_confirmed))
+
+
+
+        # Left join (to make sure to always have the same number of columns) 
+        std_deaths_confirmed <- merge(csv_countries_filename,
+                                        deaths_confirmed, 
+                                        by = "Province_State",
+                                        all.x = TRUE, 
+                                        all.y = FALSE)
+
+        # Columns names of the current report  
+        new_cols <- colnames(std_deaths_confirmed)
+
+        # Find Duplicate Column Names
+        # Remove Duplicate Column Names
+
+        duplicated_names <- duplicated(colnames(total_deaths_confirmed))
+        total_deaths_confirmed[!duplicated_names]
+
+        # Combination with previous runs
+        if !(total_deaths_confirmed.columns.isin(new_cols).any()){
+            total_deaths_confirmed <- cbind(total_deaths_confirmed, std_deaths_confirmed, check) 
+            cols_ <- colnames(total_deaths_confirmed )
+            message(paste("the columns are:", cols_) )
+            } else {
+            message("the columns already exist")
+            }
+
+        return(total_deaths_confirmed)
+        },
+        error=function(cond) {
+            
+            #
+            csv_countries_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
+            dataframe_countries<-read.delim(csv_countries_filename, sep=',')
+
+            message(paste("The file (consolidated) you are trying to merge with, may not exist"))
+            message("Original error message:",cond)
+            
+            # Merge with the reference file with country names
+           total_deaths_confirmed <- merge(dataframe_countries, 
+                        deaths_confirmed, 
+                        by = "Province_State", 
+                        all.x = TRUE, 
+                        all.y = FALSE)
+
+
+            return(deaths_confirmed)
+                })
+        return(out)
     }
-    print(args[1])
-    res = concatener()
+
+
+
+if(!interactive()){
+     
+    args = commandArgs(trailingOnly=TRUE)
+
+    res = concatener(args)
+    
     readr::write_csv(res, 
                 file=args[3])
 
