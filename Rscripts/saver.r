55f8113f33773be15657c53c37dc6a491d93cd29
Assan Sanogo <assansanogo@gmail.com>
2022-04-01 13:35:40 -0700

    Update saver.r

--- Rscripts/saver.r
+++ Rscripts/saver.r
@@ -40,7 +40,7 @@ concatener <- function(args){
         total_deaths_confirmed <- total_deaths_confirmed[,unique_names]
 
         # Combination with previous runs
-        unique_names <- subset(unique_names, !isin(unique_names, new_cols, ordered = TRUE))
+        unique_names <- subset(unique_names, !prob::isin(unique_names, new_cols, ordered = TRUE))
         total_deaths_confirmed <- total_deaths_confirmed[,unique_names]
 
         total_deaths_confirmed <- cbind(total_deaths_confirmed, std_deaths_confirmed) 
