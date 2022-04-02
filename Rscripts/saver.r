0572ffb42ff0cdf04d0ee594b71c089789684066
Assan Sanogo <assansanogo@gmail.com>
2022-04-01 17:02:00 -0700

    Update saver.r

--- Rscripts/saver.r
+++ Rscripts/saver.r
@@ -6,24 +6,39 @@ library(emo)
 library(dplyr)
 
 
-
 concatener <- function(args){
-    # current report    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # current report (for the queried date)
+    
     deaths_confirmed <- read.delim(args[1], sep=',', header=TRUE)
     
+    # reference file shipped with R package
     # external dependency (list of countries)
-    csv_states_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
-    ref_dataframe_states<-read.delim(csv_states_filename, sep=',', header=TRUE)
+    csv_states_filename<-system.file("extdata", 
+                                     "total_states_US.csv", 
+                                     package = "StreamlinR")
+    ref_dataframe_states<-read.delim(csv_states_filename, 
+                                     sep=',', 
+                                     header=TRUE)
 
-    # running report
-    total_deaths_confirmed <- read.delim(args[2], sep=',', header=TRUE)
+    # running report (includes previous days/runs)
+    total_deaths_confirmed <- read.delim(args[2], 
+                                         sep=',', 
+                                         header=TRUE)
     
     out <- tryCatch({ 
-        #debug
-        print(colnames(total_deaths_confirmed ))
-        print(colnames(deaths_confirmed))
-        print(colnames(ref_dataframe_states))
-
         # Left join (to make sure to always have the same number of columns) 
         std_deaths_confirmed <- merge(ref_dataframe_states,
                                         deaths_confirmed, 
@@ -43,15 +58,14 @@ concatener <- function(args){
         print((old_cols %in% new_cols))
 
         # Combination with previous runs
-        
+        # The columns must be checked for appending the day results only once.
         difference_in_cols <- old_cols[setdiff(old_cols, new_cols)]
-        
         
         if (!rlang::is_empty(difference_in_cols)){
             
         total_deaths_confirmed <- total_deaths_confirmed %>% dplyr::select(difference_in_cols)
-        } else{
-            message("it was previously computed")
+        } else {
+            message("the pipeline was previously computed for this day - will overwrite the previous data")
             total_deaths_confirmed <- total_deaths_confirmed %>% dplyr::select(-one_of(new_cols))
             }
         
@@ -60,32 +74,36 @@ concatener <- function(args){
         cols_ <- colnames(total_deaths_confirmed )
         message(paste(c("the columns are:", cols_)))
        
-
         return(total_deaths_confirmed)
         },
         error=function(cond) {
             
-            #
-            csv_countries_filename<-system.file("extdata", "total_countries.csv", package = "StreamlinR")
-            dataframe_countries<-read.delim(csv_countries_filename, sep=',')
+            # reference file shipped with R package
+            # referrence states dataframe
+            csv_states_filename<-system.file("extdata", 
+                                                "total_states_US.csv", 
+                                                package = "StreamlinR")
+            ref_dataframe_states<-read.delim(csv_states_filename, 
+                                            sep=',')
 
-            message(paste("The file (consolidated) you are trying to merge with, may not exist"))
+            message(paste("ERROR", 
+                          "1. The file (consolidated) you are trying to merge with, may not exist",
+                          "2. The file (consolidated) might have the wrong type of separator",
+                          sep="\n"))
+            
             message("Original error message:", cond)
             
             # Merge with the reference file with country names
-            total_deaths_confirmed <- merge(dataframe_countries, 
-                        deaths_confirmed, 
-                        by = "Province_State", 
-                        all.x = TRUE, 
-                        all.y = FALSE)
-
+            total_deaths_confirmed <- merge(ref_dataframe_states, 
+                                            deaths_confirmed, 
+                                            by = "Province_State", 
+                                            all.x = TRUE, 
+                                            all.y = FALSE)
 
             return(total_deaths_confirmed)
                 })
         return(out)
     }
-
-
 
 if(!interactive()){
      
