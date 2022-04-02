e445fd9ff700456c5524ade22037e9abb7f5551c
Assan Sanogo <assansanogo@gmail.com>
2022-04-01 20:10:09 -0700

    Update main.sh

--- Rscripts/main.sh
+++ Rscripts/main.sh
@@ -1,14 +1,11 @@
 #!/bin/sh -l
 
+#retrieving. data
 Rscript --vanilla reporter.r "$DATE" "$AGGREGATION" "$COUNTRY"
 
-echo $(ls ./out/data/US)
-
-echo $(ls ./out/data)
-
-echo $(ls ./out/US)
-
-
+#consolidating data
 Rscript --vanilla saver.r "$PROCESSED_REPORT_PATH" "$TOTAL_REPORT_PATH" "$OUTPUT_REPORT_PATH"
 
+#simplify access errors when accessing the share repository
+#from the host
 chmod -R 777 $(pwd)
